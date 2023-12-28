f767ba03738c0cc793b34bb5364b83f42300fd35
unknown <dhakadrohan98>
2023-12-28 21:33:19 +0530

    removing wrong push to the branch

--- WebGate/SMSAShipping/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-    
-    use Magento\Framework\Component\ComponentRegistrar;
-    
-    ComponentRegistrar::register(
-        ComponentRegistrar::MODULE ,
-        'WebGate_SMSAShipping' ,
-        __DIR__
-    );
